a60b6f2b6cfd09ba576ca29f4439ebecf4c353e4
pkpk123123 <55054025+pkpk123123@users.noreply.github.com>
2019-09-24 19:48:26 +0800

    gongchang
    jicheng duotai

---
+++ Yu.java
@@ -0,0 +1,41 @@
+ abstract class Tv{
+ abstract void play();
+}
+ class HaierTv extends Tv{
+	 void play(){
+		System.out.println("This is HaierTv.");
+	}
+}
+ class HvTv extends Tv{
+	 void play(){
+		System.out.println("This is HvTv.");
+	}
+}
+ class TvFactory{   //简单工厂模式
+	 public static Tv produceTv(String brand){
+			 
+		 if(brand.equals("HvTv"))
+			 return new HvTv();
+		 else if(brand.equals("HaierTv"))
+			 return new HaierTv();
+		 else
+			 return null;
+		 
+	 }
+ }
+ interface TvFactory{ //工厂方法模式
+	 public Tv produceTv();
+ }
+ class HaierTvFactory implements Tvfactory{
+	 public Tv produceTv(){
+		 return new HaierTv();
+	 }
+ }
+ class HvTvFactory implements Tvfactory{
+	 public Tv produceTv(){
+		 return new HvTv();
+	 }
+ }
+public class Yu {
+
+}

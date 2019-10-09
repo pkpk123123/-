3a6865a54449f108f6f1c924db60004289bab36a
pkpk123123 <55054025+pkpk123123@users.noreply.github.com>
2019-10-09 21:50:15 +0800

    包
    包

---
+++ Package/src/Test.java
@@ -0,0 +1,10 @@
+import course.*;
+import student.*;
+import teacher.*;
+public class Test{
+	public static void main(String[] args){
+		Course cs = new Course();
+		Student st = new Student();
+		Teacher ts = new Teacher();
+	}
+}
